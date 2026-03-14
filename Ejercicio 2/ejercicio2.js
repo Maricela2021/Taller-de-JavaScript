9c450c12782e019572e6168c94adb6150d035e2e
Maricela2021 <79016546+Maricela2021@users.noreply.github.com>
2026-03-13 20:00:56 -0500

    Agrego ejercicio 2

---
+++ Ejercicio 2/ejercicio2.js
@@ -0,0 +1,14 @@
+//crear un array con 3 colores
+const colores = ["rojo", "negro", "azul"];
+
+//crear un objeto con información de estudiante
+const estudiante = {
+    nombre: "Maricela",
+    edad: 35,
+    curso: "JavaScript",
+};
+
+console.log("lista de colores:",colores);
+console.log("Información del estudiante:",estudiante);
+
+
