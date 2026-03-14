098ee664b355ee48e5eb09fa75a31b94d57ddfa1
Maricela2021 <79016546+Maricela2021@users.noreply.github.com>
2026-03-14 17:00:11 -0500

    Agrego ejercicio 5

---
+++ Ejercicio 5/ejercicio5.js
@@ -0,0 +1,6 @@
+//seleccionar el titulo y el botón en JS e imprimelos en consola
+const titulo = document.getElementById("titulo");
+const boton = document.getElementById("btn");
+
+console.log("Titulo:", titulo);
+console.log("Botón:", boton);
