6bf17c433451431d1c62cd7c1ce54fbebd172630
Maricela2021 <79016546+Maricela2021@users.noreply.github.com>
2026-03-13 21:13:13 -0500

    Agrego ejercicio 3

---
+++ Ejercicio 3/ejercicio3.js
@@ -0,0 +1,17 @@
+// Ejemplo de valor para la nota, escenario 1: Aprobado
+let nota1 = 4; 
+
+if (nota1 >= 3) {
+    console.log("Nota:", nota1, "Aprobado");
+} else {
+    console.log("Nota:", nota1, "Reprobado");
+}
+
+// solo cambiamos el valor, escenario 2: Reprobado
+let nota2 = 2; 
+
+if (nota2 >= 3) {
+    console.log("Nota:", nota2, "Aprobado");
+} else {
+    console.log("Nota:", nota2, "Reprobado");
+}
---
+++ Ejercicio 3/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+// Ejemplo de valor para la nota, escenario 1: Aprobado
+let nota = 4; 
+
+if (nota >= 3) {
+    console.log("Nota:", nota, "Aprobado");
+} else {
+    console.log("Nota:", nota, "Reprobado");
+}
+
+// solo cambiamos el valor, escenario 2: Reprobado
+nota = 2; 
+
+if (nota >= 3) {
+    console.log("Nota:", nota, "Aprobado");
+} else {
+    console.log("Nota:", nota, "Reprobado");
+}
